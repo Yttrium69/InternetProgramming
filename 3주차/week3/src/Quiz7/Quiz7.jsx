2c218b2029564adeaebbb93dc30b04cccef27959
데스크탑 <eemune@nave.com>
2023-03-18 14:55:44 +0900

    last

--- 3주차/week3/src/Quiz7/Quiz7.jsx
+++ 3주차/week3/src/Quiz7/Quiz7.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import styled from "styled-components";
 
+
+const colorArr=["#D01F1F", "#FF650F", "#FFE600", "#00BE13", "#366EFF", "#2000E8", "#4900E3"]
+const colorIdx=[0, 1, 2, 3, 4, 5, 6]
+
+
 const Div=styled.div`
-    background-color:'blue';
+    background-color:${colorArr[(props) => props.idx]};
 `
 
 function Quiz7(props) {
     return (
         <div>
             {props.Nav}
-            <Div>가보자고</Div>
+            {colorIdx.map(
+                function(idx){
+                    return(<Div i={idx}>{colorArr[idx]}</Div>)
+                }
+            )}
         </div>
     );
 }
